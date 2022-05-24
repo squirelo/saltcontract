e6179d09920515265860918cc864f5ca39c634d8
"Valentin Squirelo" <"valentin@squirelo.com">
2022-05-24 10:25:32 +0200

    remove useles stuff

--- contracts/Salt.sol
+++ contracts/Salt.sol
@@ -1,21 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "./ERC721ATradable.sol";
 
 /**
  * @title ERC721AR
- * @author Valentin Squirelo
+ * @author 
  * @notice ERC721A + royalties + proxies.
  */
+
 contract Salt is ERC721ATradable {
     using ECDSA for bytes32;
     using Strings for uint256;
 
-    uint256 public mintPrice = 0 ether;
+    uint256 public saltValue = 0 ether;
     uint256 public maxSupply = 333;
 
     address private signerAddress = 0x1e72D4438c49f0f523E9Cbc85c33ca33Ad24B870;
@@ -23,10 +22,10 @@ contract Salt is ERC721ATradable {
     string public script;
     string public scriptType = "p5js";
 
-    event NewSaltHash(uint256 indexed salt, bytes32 indexed tokenHash);
+    event Mint(address indexed to, uint256 indexed salt);
 
     mapping(bytes32 => bool) private usedHashes;
-    mapping(uint256 => bytes32) public soulHashes;
+    mapping(uint256 => bytes32) public saltHashes;
 
     constructor(
       string memory baseTokenURI_
@@ -39,20 +38,20 @@ contract Salt is ERC721ATradable {
         uint256 salt = totalSupply();
         require(tx.origin == msg.sender, "The caller is another contract");
         require(salt < maxSupply, "Max Supply Reached");
-        require(msg.value == (mintPrice),"Wrong price");
+        require(msg.value == (saltValue),"Wrong price");
         require(verifyAddressSigner(messageHash, signature), "Signature validation has failed");
         require(!usedHashes[messageHash], "Hash has already been used");
         usedHashes[messageHash] = true;
 
         _safeMint(msg.sender, 1);
         generateSaltHash(salt);
+        emit Mint(msg.sender, salt);
     }
     
 
     function generateSaltHash(uint256 salt) internal {
-        bytes32 hash = keccak256(abi.encodePacked(block.number, block.timestamp, msg.sender));
-        soulHashes[salt]=hash;
-        emit NewSaltHash(salt, hash);
+        bytes32 tokenHash = keccak256(abi.encodePacked(block.number, block.timestamp, msg.sender));
+        saltHashes[salt]=tokenHash;
     }
 
     function setScript(string memory _script) public onlyOwner {
@@ -63,8 +62,8 @@ contract Salt is ERC721ATradable {
         script = _scriptType;
     }
 
-    function setMintingPrice(uint256 _newPrice) external onlyOwner() {
-      mintPrice = _newPrice;
+    function setSaltValue(uint256 _newPrice) external onlyOwner() {
+      saltValue = _newPrice;
     }
 
     function verifyAddressSigner(bytes32 messageHash, bytes memory signature) private view returns (bool) {
